9acf3a8a236930ded4ca16de77c7275a13f21b06
okazakihayato <okzkhyt0419waseda@outlook.jp>
2026-02-17 15:44:06 +0900

    Initial Commit

---
+++ RecipeRecommender/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipeRecommender
+//
+//  Created by 岡崎隼斗 on 2026/02/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
